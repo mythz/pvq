9908e9411574ad1ce7c9ff97e567ae8ca1bb3f62
Darren Reid <darren@reidmail.org>
2024-04-09 16:18:55 +1000

    Small fixes

--- fix-rank-dir.mjs
+++ fix-rank-dir.mjs
@@ -19,25 +19,11 @@ function processDir(dirPath) {
     const nodes = fs.readdirSync(dirPath)
     const files = nodes.filter(x => x.endsWith('.json')).filter(x => x.indexOf(`.validation.`) > -1)
     const subDirs = nodes.filter(x => !x.includes('.'))
+    const reasonIds = files.filter(x => x.endsWith(`.reasons.json`)).map(x => x.split('.')[0])
 
-    const candidates = files.filter(x => x.indexOf(`.validation.`))
+    const candidates = files.filter(x => x.indexOf(`.validation.`) && !reasonIds.includes(x.split('.')[0]))
 
     candidates.forEach(file => {
-        // Check if the votes result file already exists, and check if enough votes are present
-        // Get Id from file name
-        // const id = file.split('.')[0]
-        // // Check if the votes file already exists
-        // const votesFile = path.join(dirPath, `${id}.v.json`)
-        // if (fs.existsSync(votesFile)) {
-        //     console.log(`skipping: ${file}`)
-        //     // Read JSON, and count modelVotes keys
-        //     const data = fs.readFileSync(votesFile, 'utf8')
-        //     const jsonData = JSON.parse(data)
-        // }
-        // if (file.endsWith('.v.json')) {
-        //     // If we are here, either the votes file doesn't exist, or it already has enough votes, and doesn't need to be fixed
-        //     return
-        // }
         console.log(`${fileCount++}: ./fix-rank-file.mjs ${path.join(dirPath, file)} ${modelName} ${port}`)
         const r = execSync(`./fix-rank-file.mjs ${path.join(dirPath, file)} ${modelName} ${port}`).toString()
         console.log(r)
