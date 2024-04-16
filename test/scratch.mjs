926803f8e7cfcc0db651d89d0d4d97d3ef298182
Demis Bellot <demis.bellot@gmail.com>
2024-04-16 21:55:22 +0800

    Update scratch.mjs

--- test/scratch.mjs
+++ test/scratch.mjs
@@ -1,8 +1,19 @@
 #!/usr/bin/env node
 
-import { sleep, formatTime } from "../lib.mjs"
+import { sleep, formatTime, leftPart, lastLeftPart, lastRightPart } from "../lib.mjs"
 
 const startedAt = new Date()
 await sleep(1000)
 const elapsed = new Date() - startedAt
 console.log(formatTime(elapsed))
+
+const answerPaths = [
+    `questions/000/014/967.a.gemini-pro.json`,
+    `questions/000/014/967.a.deepseek-coder-6.7b.json`,
+    `questions/000/014/746.h.most-voted.json`,
+    `questions/000/014/746.h.accepted.json`,
+]
+
+answerPaths.forEach(answerPath => {
+    console.log(lastLeftPart(lastRightPart(answerPath, '/').substring('000.a.'.length), '.'))
+})
