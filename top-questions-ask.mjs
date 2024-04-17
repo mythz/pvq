0c647b4512f6eb4aaa2b76e0f9c8f63e1aba2a60
Darren Reid <darren@reidmail.org>
2024-04-17 14:03:40 +1000

    Skip answers that already exist.

--- top-questions-ask.mjs
+++ top-questions-ask.mjs
@@ -3,7 +3,7 @@
 import fs from "fs"
 import path from "path"
 import { execSync } from "child_process"
-import {idParts} from "./lib.mjs";
+import {idParts, openAiFromModel} from "./lib.mjs";
 
 const questionsFile = process.argv[2]
 let model = process.argv[3]
@@ -16,12 +16,20 @@ if (!questionsFile || !fs.existsSync(questionsFile)) {
     process.exit()
 }
 
+let safeModelName = openAiFromModel(model).replace(/:/g, '-')
+
 function processQuestions(questionsFile) {
     const questions = fs.readFileSync(questionsFile, 'utf-8').split('\n').filter(x => x.trim().length > 0)
     questions.forEach((questionId, i) => {
         // Find the question file from the questionId
         // 000.json
         let questionPath = path.join(idParts(questionId).questionPath)
+        // Check if answer file exists
+        let answerPath = path.join(idParts(questionId).questionPath.replace('.json', `.a.${safeModelName}.json`))
+        if (fs.existsSync(answerPath)) {
+            console.log(`Answer already exists: ${i}: ${questionId} ${model}`)
+            return
+        }
         console.log(`${i}: ./ask.mjs "${questionId}" ${model}`)
         try {
             const r = execSync(`./ask.mjs "${questionPath}" ${model} ${port}`, { timeout: 60*1000 }).toString()
