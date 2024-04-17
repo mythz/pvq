177dbe37a603bad174530c2478d34ee5532f831b
Darren Reid <darren@reidmail.org>
2024-04-17 12:03:20 +1000

    Simple loop for asking top questions from a line separate list of question IDs.

---
+++ top-questions-ask.mjs
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+import fs from "fs"
+import path from "path"
+import { execSync } from "child_process"
+import {idParts} from "./lib.mjs";
+
+const questionsFile = process.argv[2]
+let model = process.argv[3]
+let port = process.argv[4] ?? '11434'
+
+if (!model) throw "model required"
+
+if (!questionsFile || !fs.existsSync(questionsFile)) {
+    console.log('questionsFile does not exist', questionsFile)
+    process.exit()
+}
+
+function processQuestions(questionsFile) {
+    const questions = fs.readFileSync(questionsFile, 'utf-8').split('\n').filter(x => x.trim().length > 0)
+    questions.forEach((questionId, i) => {
+        // Find the question file from the questionId
+        // 000.json
+        let questionPath = path.join(idParts(questionId).questionPath)
+        console.log(`${i}: ./ask.mjs "${questionId}" ${model}`)
+        try {
+            const r = execSync(`./ask.mjs "${questionPath}" ${model} ${port}`, { timeout: 60*1000 }).toString()
+            console.log(r)
+        } catch(e) {
+            console.log('ERROR execSync:', e)
+        }
+    })
+}
+
+processQuestions(questionsFile)
