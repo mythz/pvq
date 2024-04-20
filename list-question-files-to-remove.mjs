6e5e631101d3793c192327fcfa22777c43936a4c
Demis Bellot <demis.bellot@gmail.com>
2024-04-20 14:34:47 +0800

    Create list-question-files-to-remove.mjs

---
+++ list-question-files-to-remove.mjs
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import fs from "fs"
+import path from "path"
+
+const dir = process.argv[2] || './questions'
+
+function processDir(dir) {
+    const nodes = fs.readdirSync(dir)
+    const files = nodes.filter(x => x.includes('.'))
+    const subDirs = nodes.filter(x => !x.includes('.'))
+
+    files.forEach(file => {
+        if (file.endsWith('.json')) {
+            if (file.length == '000.json'.length) return
+            if (file.length == '000.meta.json'.length && file.endsWith('.meta.json')) return
+            if (file.length > 10) {
+                const answerSuffix = file.substring(3)
+                if (answerSuffix.startsWith('.a.') || answerSuffix.startsWith('.h.')) return
+            }
+        }
+        console.log(path.join(dir, file))
+    })
+
+    subDirs.forEach(subDir => processDir(path.join(dir,subDir)))
+}
+
+processDir(dir)
