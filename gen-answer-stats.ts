4506b9e3f5422226f8e88385a9d429375bc9d5e8
Demis Bellot <demis.bellot@gmail.com>
2024-04-21 17:56:44 +0800

    Add script to generate answer stats of all answers to check for bad ones

---
+++ gen-answer-stats.ts
@@ -0,0 +1,119 @@
+#!/usr/bin/env bun
+
+/**
+ * This script creates the answers-stats.db containing word and character statistics about each answer
+ */
+
+import { Database } from 'bun:sqlite'
+import fs from 'fs'
+import path from 'path'
+import { lastLeftPart, extractIdFromPath, createErrorLog, getAnswerBody } from "./lib.mjs"
+import { contentStats, Stat } from './lib-view.ts'
+
+const dir = process.argv[2] || './questions'
+
+const dbPath = './dist/answers-stats.db'
+
+if (fs.existsSync(dbPath)) {
+    fs.rmSync(dbPath)
+}
+
+const db = new Database(dbPath)
+
+// Id = AnswerId
+db.exec(`CREATE TABLE Stat (
+    Id text primary key,
+    PostId integer not null,
+    AnswerPath text not null,
+    Length integer not null,
+    MaxChar text null,
+    MaxCharCount text not null,
+    MaxWord text null,
+    MaxWordCount text not null,
+    NonAsciiCount integer not null,
+    CharTotals text not null,
+    WordTotals text not null
+)`)
+
+const insert = db.prepare(
+    `INSERT INTO Stat (Id,PostId,AnswerPath,Length,MaxChar,MaxCharCount,MaxWord,MaxWordCount,NonAsciiCount,CharTotals,WordTotals) VALUES ` +
+    `($id,$postId,$answerPath,$length,$maxChar,$maxCharCount,$maxWord,$maxWordCount,$nonAsciiCount,$charTotals,$wordTotals)`)
+const tasks:any[] = []
+
+const logError = createErrorLog(process.argv[1], { reset:true })
+
+let count = 0
+
+function processDir(dir) {
+    const nodes = fs.readdirSync(dir)
+    const files = nodes.filter(x => x.includes('.'))
+    const subDirs = nodes.filter(x => !x.includes('.'))
+
+    files.forEach(file => {
+        if (file.endsWith('.json') && file.length > 10) {
+            const answerSuffix = file.substring(3)
+            if (!answerSuffix.startsWith('.a.')) return
+            
+            const answerPath = path.join(dir, file)
+            try {
+                const answerJson = fs.readFileSync(answerPath, 'utf8')
+                const postId = extractIdFromPath(answerPath)
+                const userName = lastLeftPart(file.substring('000.a.'.length), '.')
+                const id = `${postId}-${userName}`
+                const body = getAnswerBody(answerJson)
+                if (!body || body.trim().length === 0) {
+                    insert.run({
+                        id,
+                        postId,
+                        answerPath,
+                        length: 0,
+                        maxChar: '',
+                        maxCharCount: 0,
+                        maxWord: '',
+                        maxWordCount: 0,
+                        nonAsciiCount: 0,
+                        charTotals: '{}',
+                        wordTotals: '{}',
+                    })
+                    logError(`${answerPath} empty body`)
+                    return
+                }
+                const stats = contentStats(body)
+                const row = {
+                    id,
+                    postId,
+                    answerPath,
+                    ...stats,
+                }
+                const dbRow = {}
+                for (const key in row) {
+                    dbRow['$' + key] = row[key]
+                }
+                dbRow['$charTotals'] = JSON.stringify(stats.charTotals)
+                dbRow['$wordTotals'] = JSON.stringify(stats.charTotals)
+                try {
+                    insert.run(dbRow)
+                    count++
+                } catch(e) {
+                    console.log(`failed to insert stat ${id} in ${answerPath}`, e)
+                    console.log(dbRow)
+                    return
+                }
+                if (count % 1000 === 0) {
+                    console.log(count, row)
+                }
+            } catch(e) {
+                logError(`${answerPath} ${e.message}`)
+                console.log(answerPath)
+            }
+        }
+    })
+
+    subDirs.forEach(subDir => processDir(path.join(dir,subDir)))
+}
+
+processDir(dir)
+
+insert.finalize()
+db.close(true)
+
---
+++ lib-view.ts
@@ -0,0 +1,83 @@
+import { stopWords } from './data.mjs'
+
+export interface Stat {
+    Id: string
+    PostId: number
+    AnswerPath: string
+    Length: number
+    MaxChar: string
+    MaxCharCount: number
+    MaxWord: string
+    MaxWordCount: number
+    NonAsciiCount: number
+    CharTotals: string
+    WordTotals: string
+}
+
+const checkChars = ['.',',','-','_',';','|','\n','\t','/','\\','`','-','+','[',']','(',')']
+
+export function contentStats(body:string) {
+    const allCharTotals = {}
+    for (let i=0; i<body.length; i++) {
+        const char = body[i]
+        if (checkChars.includes(char)) {
+            if (!allCharTotals[char]) allCharTotals[char] = 0
+            allCharTotals[char]++
+        }
+    }
+    const nonAsciiCount = body.length - body.replace(/[\u{0080}-\u{FFFF}]/gu,"").length
+    let maxChar = ''
+    let maxCharCount = 0
+    for (const char in allCharTotals) {
+        if (allCharTotals[char] > maxCharCount) {
+            maxCharCount = allCharTotals[char]
+            maxChar = char
+        }
+    }
+    const charTotals = sortTotals(allCharTotals, 10)
+        .filter(x => x.value > 2)
+        .reduce((acc, x) => {
+            acc[x.key] = x.value
+            return acc
+        }, {})
+
+    let maxWord = ''
+    let maxWordCount = 0
+    let words = body.replace(/[\u{0080}-\u{FFFF}]/gu," ").split(/\s+/g)
+        .filter(x => x.length > 1 && !stopWords.has(x))
+    const allWordTotals = {
+    }
+    for (const rawWord of words) {
+        const word = rawWord.replace(/['"`]/g,'')
+        if (!allWordTotals[word]) allWordTotals[word] = 0
+        allWordTotals[word]++
+
+        if (allWordTotals[word] > maxWordCount) {
+            maxWordCount = allWordTotals[word]
+            maxWord = word
+        }
+    }
+    const wordTotals = sortTotals(allWordTotals, 10)
+        .filter(x => x.value > 2)
+        .reduce((acc, x) => {
+            acc[x.key] = x.value
+            return acc
+        }, {})
+
+    return {
+        length: body.length,
+        maxChar,
+        maxCharCount,
+        maxWord,
+        maxWordCount,
+        nonAsciiCount,
+        charTotals,
+        wordTotals,
+    }
+}
+
+export function sortTotals(totals:{[key:string]:number}, take:number = 10) {
+    return Object.keys(totals).sort((a,b) => totals[b] - totals[a])
+        .slice(0,take)
+        .map(key => ({key, value: totals[key]}))
+}
