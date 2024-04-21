85b0cefd7ae59fa5075cfbf683498b5bbe34632b
Demis Bellot <demis.bellot@gmail.com>
2024-04-21 17:57:23 +0800

    Add ability to check answers-stats.db database

---
+++ db-answers.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env bun
+
+/**
+ * This script allows you to query the answers-stats.db database, e.g:
+ * ./db-answers.ts                            # prints the top 10 answers by length
+ * ./db-answers.ts -orderBy Length            # prints the top 10 answers by smallest length
+ * ./db-answers.ts -orderByDesc MaxCharCount  # prints the top 10 answers by Max Char Count
+ * ./db-answers.ts -where "PostId"   # prints only Answer Id and Path
+ * ./db-answers.ts -select "Id, AnswerPath"   # prints only Answer Id and Path
+ * ./db-answers.ts -take 20                   # prints the top 20 answers by length
+ * ./db-answers.ts -skip 20 -take 20          # prints the next top 20 answers by length
+ */
+
+import { Database } from 'bun:sqlite'
+import { Inspect } from "./@servicestack/client"
+import { Stat } from './lib-view.ts'
+
+const dbPath = './dist/answers-stats.db'
+
+const args = process.argv.slice(2)
+const cmds:{[name:string]:any} = {}
+for (let i = 0; i < args.length; i++) {
+    let name = args[i]
+    if (name.startsWith('-')) {
+        name = name.substring(1)
+    }
+    cmds[name] = args[i + 1]
+    i++
+}
+
+const db = new Database(dbPath)
+
+const select = 'Id, PostId, Length, MaxCharCount, MaxWord, MaxWordCount, NonAsciiCount'
+const orderBy = cmds.orderBy || cmds.orderByDesc ? `${cmds.orderByDesc} DESC` : 'Length DESC'
+const where = cmds.where || '1=1'
+const skip = cmds.skip || 0
+const take = cmds.take || 10
+const stmt = db.prepare(`SELECT ${select} FROM Stat WHERE ${where} ORDER BY ${orderBy} LIMIT ${take} OFFSET ${skip}`)
+const rows = stmt.all()
+
+Inspect.printDumpTable(rows)
+stmt.finalize()
---
+++ view.ts
@@ -0,0 +1,124 @@
+#!/usr/bin/env bun
+
+/**
+ * This script views and answer or question by id or path
+ */
+
+import fs from 'fs'
+import path from 'path'
+import { lastLeftPart, extractIdFromPath, lastRightPart, getAnswerBody, idParts, rightPart, leftPart } from "./lib.mjs"
+
+const target = process.argv[2] 
+const verbose = process.argv[3] === '-v' 
+
+import { contentStats } from './lib-view.ts'
+
+if (!target) {
+    console.log(`Usage: ./${lastRightPart(process.argv[1],'/')} <post-id|answer-id|path>`)
+    process.exit(1)
+}
+
+let id = target.includes('.') 
+    ? extractIdFromPath(target)?.toString()
+    : target
+
+if (!id) {
+    console.error('invalid id', target)
+    process.exit(1)
+}
+
+if (target.includes('.a.') || target.includes('.h.')) {
+    id += `-${lastLeftPart(lastRightPart(target,'/')?.substring('000.a.'.length),'.')}`
+}
+
+const isAnswer = id.includes('-')
+
+const postId = parseInt(leftPart(id, '-'))
+const { dir1, dir2, fileId, file, questionDir, metaDir, questionPath, metaPath, vPath } = idParts(postId)
+if (!fs.existsSync(questionPath)) {
+    console.error(`Question ${id}: ${questionPath} not found`)
+    process.exit(1)
+}
+if (!fs.existsSync(metaPath)) {
+    console.error(`Question ${id}: ${metaPath} not found`)
+    process.exit(1)
+}
+const metaJson = fs.readFileSync(metaPath, 'utf-8')
+const meta = JSON.parse(metaJson)
+const { modelVotes, modelReasons, gradedBy, statTotals, comments } = meta
+
+const questionJson = fs.readFileSync(questionPath, 'utf-8')
+const question = JSON.parse(questionJson)
+
+if (isAnswer) {
+    const userName = rightPart(id, '-')
+    const modelAnswerPath = path.join(questionDir, `${fileId}.a.${userName}.json`)
+    const humanAnswerPath = path.join(questionDir, `${fileId}.h.${userName}.json`)
+    const answerPath = fs.existsSync(modelAnswerPath) 
+        ? modelAnswerPath 
+        : fs.existsSync(modelAnswerPath) 
+            ? humanAnswerPath
+            : null
+    if (!answerPath) {
+        console.error(`Answer not found: ${id}`)
+        process.exit(1)
+    }
+
+    const answerJson = fs.readFileSync(answerPath, 'utf-8')
+    const answer = JSON.parse(answerJson)
+    const body = getAnswerBody(answerJson)
+
+    console.log(`\nAnswer: ${id}`)
+    console.log(`-`.repeat(80))
+    console.log(body?.trim())
+    console.log(`-`.repeat(80))
+    const createdDate = answer.created 
+        ? new Date(answer.created * 1000).toDateString()
+        : new Date(answer.creationDate).toDateString()
+    console.log(`created by ${userName} at ${createdDate}`)
+
+    console.log(`\nFile: ${answerPath}`)
+    console.log(`Post: ${questionPath}`)
+    console.log(`Meta: ${metaPath}`)
+    console.log(`Url: https://pvq.app/questions/${question.id}/${question.slug}#${id}`)
+
+    if (verbose) {
+        const statTotal = (statTotals ?? []).find(x => x.id === id)
+        console.log(`\nTotals:`, statTotal)
+        const stats = contentStats(body)
+        console.log(`\nBody Stats:`)
+        console.log(stats)
+    }
+} else {
+    console.log(`\nQuestion: ${id}`)
+    console.log(`Title:`, question.title)
+    console.log(`Tags:`, question.tags)
+    console.log(`-`.repeat(80))
+    console.log(question.body?.trim())
+    console.log(`-`.repeat(80))
+    console.log(`created by ${question.createdBy} at ${new Date(question.creationDate).toDateString()}, ${question.viewCount} views`)
+
+    console.log(`\nPost: ${questionPath}`)
+    console.log(`Meta: ${metaPath}`)
+    console.log(`Url: https://pvq.app/questions/${question.id}/${question.slug}`)
+    console.log(`\nAnswers:`)
+    const answersFiles = fs.readdirSync(questionDir)
+        .filter(x => x.startsWith(`${fileId}.a.`) || x.startsWith(`${fileId}.h.`))
+    const answers = answersFiles.map(x => {
+        const file = lastRightPart(x, '/')
+        const userName = lastLeftPart(file.substring('000.a.'.length), '.')
+        const score = modelVotes && modelVotes[userName] ? modelVotes[userName] : ' '
+        return { score, x }
+    })
+    answers.sort((a,b) => b.score - a.score)
+        .forEach(({ score, x }) => console.log(score, path.join(questionDir, x)))
+
+    if (verbose) {
+        const statTotal = (statTotals ?? []).find(x => x.id === id)
+        console.log(`\nTotals:`, statTotal)
+        const stats = contentStats(question.body)
+        console.log(`\nBody Stats:`)
+        console.log(stats)
+    }
+}
+
